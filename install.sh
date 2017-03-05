1b033315fee54412788afe45f8f60764164f87c1
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-03-05 21:08:23 +0100

    fixing script name in installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-wget -O noti.py https://raw.githubusercontent.com/pwittchen/spotify-cli-linux/master/spotify-cli
+wget -O spotify-cli https://raw.githubusercontent.com/pwittchen/spotify-cli-linux/master/spotify-cli
 chmod +x noti.py
-sudo mv noti.py /usr/local/bin/spotify-cli
+sudo mv spotify-cli /usr/local/bin/spotify-cli
