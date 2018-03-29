1231d5c47dc9482efe4f62de829ff43a56ade80e
Piotr Wittchen <piotr@wittchen.biz.pl>
2018-03-29 22:58:45 +0200

    adding missing comma in setup.py script

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = '0.0.1',
     description = 'A command-line interface to Spotify on Linux',
     author = 'pwittchen',
-    author_email = 'piotr.wittchen@gmail.com'
+    author_email = 'piotr.wittchen@gmail.com',
     url = 'https://github.com/pwittchen/spotify-cli-linux',
     py_modules=['spotify-cli-linux'],
     install_requires=[
