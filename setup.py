30814abe9956a3bb8a1a800187337951a7f39ec8
Piotr Wittchen <piotr@wittchen.biz.pl>
2018-03-29 23:44:18 +0200

    updating version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'spotify-cli-linux',
-    version = '0.0.1',
+    version = '0.0.3',
     description = 'A command-line interface to Spotify on Linux',
     author = 'pwittchen',
     author_email = 'piotr.wittchen@gmail.com',
