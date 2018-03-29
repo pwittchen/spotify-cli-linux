0ec25f409ff11ea59f9b3ba3c329352c5a05000b
Piotr Wittchen <piotr@wittchen.biz.pl>
2018-03-29 22:54:57 +0200

    fixing typo in install.sh script

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 set -e
 wget -O spotifycli https://raw.githubusercontent.com/pwittchen/spotify-cli-linux/master/spotifycli.py
 chmod +x spotifycli.py
-sudo mv spotifycli.pl /usr/local/bin/spotifycli
+sudo mv spotifycli.py /usr/local/bin/spotifycli
