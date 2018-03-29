fd0634fe6bd493b3ea31c5fa04c3c759eaec5cc4
Piotr Wittchen <piotr@wittchen.biz.pl>
2018-03-29 22:57:05 +0200

    updating install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 wget -O spotifycli https://raw.githubusercontent.com/pwittchen/spotify-cli-linux/master/spotifycli.py
-chmod +x spotifycli.py
-sudo mv spotifycli.py /usr/local/bin/spotifycli
+chmod +x spotifycli
+sudo mv spotifycli /usr/local/bin/spotifycli
