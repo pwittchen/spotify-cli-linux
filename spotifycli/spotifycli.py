40ad9df4585b2046831e7d984e5916cd08dd9297
Piotr Wittchen <piotr@wittchen.io>
2018-12-15 23:20:29 +0100

    replacing getopt with argparse

--- spotifycli/spotifycli.py
+++ spotifycli/spotifycli.py
@@ -1,136 +1,100 @@
 #!/usr/bin/python -u
 # -*- coding: utf-8 -*-
+"""a command line interface to Spotify on Linux"""
 
 import sys
-import getopt
+import argparse
 import dbus
 from subprocess import Popen, PIPE
 from version import __version__
 
 
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "", ["help", "status",
-                                                      "status-short",
-                                                      "song", "song-short",
-                                                      "album", "artist",
-                                                      "artist-short",
-                                                      "playback-status",
-                                                      "play", "pause",
-                                                      "playpause", "next",
-                                                      "prev", "volumeup",
-                                                      "volumedown",
-                                                      "version"])
-    except getopt.GetoptError:
-        show_help()
-        sys.exit(2)
+    args = add_arguments()
+    if   args.version        : show_version()
+    elif args.status         : show_status()
+    elif args.statusshort    : show_status_short()
+    elif args.song           : show_song()
+    elif args.songshort      : show_song_short()
+    elif args.artist         : show_artist()
+    elif args.artistshort    : show_artist_short()
+    elif args.album          : show_album()
+    elif args.playbackstatus : show_playback_status()
+    elif args.play           : perform_spotify_action("Play")
+    elif args.pause          : perform_spotify_action("Pause")
+    elif args.playpause      : perform_spotify_action("PlayPause")
+    elif args.next           : perform_spotify_action("Next")
+    elif args.prev           : perform_spotify_action("Previous")
+    elif args.volumeup       : control_volume("+5%")
+    elif args.volumedown     : control_volume("-5%")
 
-    for opt, arg in opts:
-        if opt in ("--help"):
-            show_help()
-        if opt in ("--version"):
-            show_version()
-        elif opt == "--status":
-            show_current_status()
-        elif opt == "--status-short":
-            show_current_status_short()
-        elif opt == "--song":
-            show_current_song()
-        elif opt == "--song-short":
-            show_current_song_short()
-        elif opt == "--artist":
-            show_current_artist()
-        elif opt == "--artist-short":
-            show_current_artist_short()
-        elif opt == "--album":
-            show_current_album()
-        elif opt == "--playback-status":
-            show_current_playback_status()
-        elif opt == "--play":
-            perform_spotify_action("Play")
-        elif opt == "--pause":
-            perform_spotify_action("Pause")
-        elif opt == "--playpause":
-            perform_spotify_action("PlayPause")
-        elif opt == "--next":
-            perform_spotify_action("Next")
-        elif opt == "--prev":
-            perform_spotify_action("Previous")
-        elif opt == "--volumeup":
-            control_volume("+5%")
-        elif opt == "--volumedown":
-            control_volume("-5%")
-
-
-def show_help():
-    print(
-        '\n  spotify-cli is a command line interface for Spotify on Linux\n\n'
-        '  usage:\n'
-        '    --help\t\tshows help\n'
-        '    --version\t\tshows version\n'
-        '    --status\t\tshows status (song name and artist)\n'
-        '    --status-short\tshows status in a short way\n'
-        '    --song\t\tshows the current song name\n'
-        '    --song-short\tshows the current song name in a short way\n'
-        '    --artist\t\tshows the current artist\n'
-        '    --artist-short\tshows the current artist in a short way\n'
-        '    --album\t\tshows the current album\n'
-        '    --playback-status\tshows the current playback status (UTF-8)\n'
-        '    --play\t\tplays the song\n'
-        '    --pause\t\tpauses the song\n'
-        '    --playpause\t\tplays or pauses the song (toggles a state)\n'
-        '    --next\t\tplays the next song\n'
-        '    --prev\t\tplays the previous song\n'
-        '    --volumeup\t\tincreases sound volume\n'
-        '    --volumedown\tdecreases sound volume\n')
+def add_arguments():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--version", help="shows version number", action="store_true")
+    parser.add_argument("--status", help="shows song name and artist", action="store_true")
+    parser.add_argument("--statusshort", help="shows status in a short way", action="store_true")
+    parser.add_argument("--song", help="shows the song name", action="store_true")
+    parser.add_argument("--songshort", help="shows the song name in a short way", action="store_true")
+    parser.add_argument("--artist", help="shows artists name", action="store_true")
+    parser.add_argument("--artistshort", help="shows artist name in a short way", action="store_true")
+    parser.add_argument("--album", help="shows album name", action="store_true")
+    parser.add_argument("--playbackstatus", help="shows playback status", action="store_true")
+    parser.add_argument("--play", help="plays the song", action="store_true")
+    parser.add_argument("--pause", help="pauses the song", action="store_true")
+    parser.add_argument("--playpause", help="plays or pauses the song (toggles a state)", action="store_true")
+    parser.add_argument("--next", help="plays the next song", action="store_true")
+    parser.add_argument("--prev", help="plays the previous song", action="store_true")
+    parser.add_argument("--volumeup", help="increases sound volume", action="store_true")
+    parser.add_argument("--volumedown", help="decreases sound volume", action="store_true")
+    return parser.parse_args()
 
 
 def show_version():
     print(__version__)
 
 
-def get_current_song():
+def get_song():
     metadata = get_spotify_property("Metadata")
     artist = metadata['xesam:artist'][0]
     title = metadata['xesam:title']
     return (artist, title)
 
 
-def show_current_status():
-    artist, title = get_current_song()
+def show_status():
+    artist, title = get_song()
     print("%s - %s" % (artist, title))
 
 
-def show_current_status_short():
-    artist, title = get_current_song()
+def show_status_short():
+    artist, title = get_song()
     artist = artist[:15] + (artist[15:] and '...')
     title = title[:10] + (title[10:] and '...')
     print("%s - %s" % (artist, title))
 
 
-def show_current_song():
-    _, title = get_current_song()
+def show_song():
+    _, title = get_song()
     print("%s" % title)
 
 
-def show_current_song_short():
-    _, title = get_current_song()
+def show_song_short():
+    _, title = get_song()
     title = title[:10] + (title[10:] and '...')
     print("%s" % title)
 
 
-def show_current_artist():
-    artist, _ = get_current_song()
+def show_artist():
+    artist, _ = get_song()
     print("%s" % artist)
 
 
-def show_current_artist_short():
-    artist, _ = get_current_song()
+def show_artist_short():
+    artist, _ = get_song()
     artist = artist[:15] + (artist[15:] and '...')
     print("%s" % artist)
 
 
-def show_current_playback_status():
+def show_playback_status():
     playback_status = get_spotify_property("PlaybackStatus")
     print({"Playing": '▶',
            "Paused": '▮▮',
@@ -138,7 +102,7 @@ def show_current_playback_status():
            }[playback_status])
 
 
-def show_current_album():
+def show_album():
     metadata = get_spotify_property("Metadata")
     print("%s" % metadata['xesam:album'])
 
