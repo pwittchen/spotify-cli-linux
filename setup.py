6aba3e5db4ae2a2f02e3171b60662443f5d012eb
Piotr Wittchen <piotr@wittchen.io>
2018-12-15 17:48:56 +0100

    updating license in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'pwittchen',
     author_email = 'piotr.wittchen@gmail.com',
     url = 'https://github.com/pwittchen/spotify-cli-linux',
-    license = 'Apache 2.0',
+    license = 'GPL 3.0',
     packages = ['spotifycli'],
     entry_points = {
        "console_scripts": ['spotifycli = spotifycli.spotifycli:main']
