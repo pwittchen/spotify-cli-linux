315dc9a900cc923bf98eb326bd2d26e9bd8bac87
Piotr Wittchen <piotr@wittchen.io>
2019-01-05 12:14:25 +0100

    updating setup.py (adding long_description) and simplifying release task in Makefile

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@ dist:
 	sudo python setup.py sdist
 upload:
 	twine upload dist/*
-release:
-	sudo rm MANIFEST || true && sudo rm -rf dist/ || true && sudo python setup.py sdist && twine upload dist/*
+release: clean
+	sudo python setup.py sdist && twine upload dist/*
 clean:
-	sudo rm MANIFEST || true && sudo rm -rf dist/ || true
+	sudo rm MANIFEST || true && sudo rm -rf dist/ || true && sudo rm -rf spotify_cli_linux.egg-info || true
 format:
 	autopep8 --in-place --aggressive spotifycli/spotifycli.py
 check_format:
--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
+from setuptools import setup
 from distutils.core import setup
 from spotifycli.version import __version__
 from spotifycli.spotifycli import __doc__
 
+with open("README.md", "r") as readme_content:
+    long_description = readme_content.read()
+
 setup(
-    name = 'spotify-cli-linux',
-    version = __version__,
-    description = __doc__,
-    author = 'pwittchen',
-    author_email = 'piotr.wittchen@gmail.com',
-    url = 'https://github.com/pwittchen/spotify-cli-linux',
-    license = 'GPL 3.0',
-    packages = ['spotifycli'],
-    entry_points = {
-       "console_scripts": ['spotifycli = spotifycli.spotifycli:main']
+    name='spotify-cli-linux',
+    version=__version__,
+    description=__doc__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='pwittchen',
+    author_email='piotr.wittchen@gmail.com',
+    url='https://github.com/pwittchen/spotify-cli-linux',
+    license='GPL 3.0',
+    packages=['spotifycli'],
+    entry_points={
+        "console_scripts": ['spotifycli = spotifycli.spotifycli:main']
     },
 )
